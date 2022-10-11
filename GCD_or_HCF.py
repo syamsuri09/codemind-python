d6a36c925479597a07bc6b800092bdcfc614f024
syam <84723010+syamsuri09@users.noreply.github.com>
2022-10-11 10:54:54 +0530

    a,b = map(int,input().split())
    while a != 0 and b != 0:
        if a > b:
            a %= b
        else:
            b %= a
    gcd = a if b == 0 else b
    print(gcd)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,9 +1,9 @@
 a,b = map(int,input().split())
-while a < b:
-    a,b = b,a
-c = a
-while True:
-    if a%c == 0 and b%c == 0:
-        print(c)
-        break
-    c -=1
+
+while a != 0 and b != 0:
+    if a > b:
+        a %= b
+    else:
+        b %= a
+gcd = a if b == 0 else b
+print(gcd)
