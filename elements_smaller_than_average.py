a85d1ab44f19b3919c247956fa886af414703e09
syam <84723010+syamsuri09@users.noreply.github.com>
2022-10-13 13:41:05 +0530

    n = int(input()) l = list(map(int,input().split()))
    count = 0
    for i in l:
        count+=i
    total = count // len(l)
    sum = 0
    for i in l:
        if i <= total:
            sum +=1
    print(sum)

---
+++ elements_smaller_than_average.py
@@ -0,0 +1,12 @@
+n = int(input())
+l = list(map(int,input().split()))
+
+count = 0
+for i in l:
+    count+=i
+total = count // len(l)
+sum = 0
+for i in l:
+    if i <= total:
+        sum +=1
+print(sum)
