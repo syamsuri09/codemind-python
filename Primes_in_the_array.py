6b2dae2f895bf25717ceae8fefee900b1e0dd5e4
syam <84723010+syamsuri09@users.noreply.github.com>
2022-10-14 22:55:04 +0530

    n = int(input()) l = list(map(int,input().split()))
    c = 0
    for i in l:
        s = True
        for j in range(2,i):
            if i % j == 0:
                s = False
                break
        if s == True and i > 1:
            c +=1
    print(c)

---
+++ Primes_in_the_array.py
@@ -0,0 +1,13 @@
+n = int(input())
+l = list(map(int,input().split()))
+
+c = 0
+for i in l:
+    s = True
+    for j in range(2,i):
+        if i % j == 0:
+            s = False
+            break
+    if s == True and i > 1:
+        c +=1
+print(c)
