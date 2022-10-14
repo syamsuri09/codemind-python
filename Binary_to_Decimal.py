37def0a7eacb40fbe55fea3b537a9cb3e5e5504d
syam <84723010+syamsuri09@users.noreply.github.com>
2022-10-14 20:30:32 +0530

    a = int(input())
    for i in range(a):
        n = input()
        n = n[::-1]
        
        c = 0
        for i in range(len(n)):
            a = 2 ** i
            if n[i] == "1":
                c+=a
        print(c)

--- Binary_to_Decimal.py
+++ Binary_to_Decimal.py
@@ -3,10 +3,10 @@ a = int(input())
 for i in range(a):
     n = input()
     n = n[::-1]
+    
     c = 0
     for i in range(len(n)):
         a = 2 ** i
         if n[i] == "1":
             c+=a
     print(c)
-    
