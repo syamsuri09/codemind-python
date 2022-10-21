a2017a3089185723ad41c64c4f8a11d4c974232a
syam <84723010+syamsuri09@users.noreply.github.com>
2022-10-21 21:30:08 +0530

    m,n = map(int,input().split()) a = list(map(int,input().split())) b = list(map(int,input().split()))
    a1 = []
    for i in a:
        if i not in a1:
            a1.append(i)
    b1 = []
    for i in b:
        if i not in b1:
            b1.append(i)
    c = []
    for i in a1:
        if i in b1:
            c.append(i)
    print(*c)

--- Common_elements.py
+++ Common_elements.py
@@ -1,12 +1,12 @@
 m,n = map(int,input().split())
 a = list(map(int,input().split()))
 b = list(map(int,input().split()))
-a1=[]
+
+a1 = []
 for i in a:
-    if(i not in a1):
+    if i not in a1:
         a1.append(i)
-        
-b1=[]
+b1 = []
 for i in b:
     if i not in b1:
         b1.append(i)
