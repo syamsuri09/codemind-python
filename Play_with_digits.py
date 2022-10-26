bc6c03f270c11a5d16ced4ed9b13dd61d256820f
syam <84723010+syamsuri09@users.noreply.github.com>
2022-10-26 21:05:13 +0530

    n = int(input()) arr = list(map(int,input().split()))
    c = ""
    for i in arr:
        i = str(i)
        c +=i
    d = 0
    for j in c:
        d +=int(j)
    print(d)

--- Play_with_digits.py
+++ Play_with_digits.py
@@ -1,10 +1,11 @@
 n = int(input())
 arr = list(map(int,input().split()))
-s=0
+
+c = ""
 for i in arr:
-    su=0
-    while i:
-        su+=i%10
-        i//=10
-    s+=su
-print(s)
+    i = str(i)
+    c +=i
+d = 0
+for j in c:
+    d +=int(j)
+print(d)
