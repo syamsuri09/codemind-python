edcf1141d8e7acccf39533a9142e2618a9cf0251
syam <84723010+syamsuri09@users.noreply.github.com>
2022-10-26 21:25:14 +0530

    n = int(input()) arr = list(map(int,input().split()))
    for i in range(len(arr)-1):
        t = arr[i] 
        s = arr[i+1]
        if t >= s:
            print("no")
            break
    else:
        print("yes")

---
+++ Ascending_array.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int,input().split()))
+
+for i in range(len(arr)-1):
+    t = arr[i] 
+    s = arr[i+1]
+    if t >= s:
+        print("no")
+        break
+else:
+    print("yes")
