bc7815dc1e47b7cbd44b24f20d235548a28add44
syam <84723010+syamsuri09@users.noreply.github.com>
2022-10-28 20:12:04 +0530

    n = list(map(str,input().split())) lower = ["a","e","i","o","u"] l = [] for i in range(len(n)):     c = 0     for j in n[i]:         if j in lower:             c+=1     l.append(c) t = max(l) c = 0 for i in l:     if t == i:         c +=1 print(c)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,15 @@
+n = list(map(str,input().split()))
+lower = ["a","e","i","o","u"]
+l = []
+for i in range(len(n)):
+    c = 0
+    for j in n[i]:
+        if j in lower:
+            c+=1
+    l.append(c)
+t = max(l)
+c = 0
+for i in l:
+    if t == i:
+        c +=1
+print(c)
