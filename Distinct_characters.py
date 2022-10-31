26cba2c2c94c5b867121b1335e868b72684ae90c
syam <84723010+syamsuri09@users.noreply.github.com>
2022-10-31 19:59:27 +0530

    n = input() n = n.lower() c = '' for i in n:     if i != " ":         c+=i d = '' for j in c:     if (c.count(j)==1):         d+=j e = '' l = sorted(d) for z in l:     e+=z print(e)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,11 +1,15 @@
 n = input()
 n = n.lower()
-c = []
+c = ''
 for i in n:
-    if i not in c and i != " ":
-        c.append(i)
-l = sorted(c)
-d =''
-for j in l:
-    d +=j
-print(d)
+    if i != " ":
+        c+=i
+d = ''
+for j in c:
+    if (c.count(j)==1):
+        d+=j
+e = ''
+l = sorted(d)
+for z in l:
+    e+=z
+print(e)
