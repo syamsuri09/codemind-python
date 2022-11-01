8f25bf94ea507d3a127799b4c55f8b6f93ce2077
syam <84723010+syamsuri09@users.noreply.github.com>
2022-11-01 20:57:11 +0530

    m = input() m = m.lower() n = input() n = n.lower()
    def two_strings(a):
        s = ""
        for i in a:
            if i !=" ":
                s+=i
        return s
    
    a = two_strings(m)
    b = two_strings(n)
    
    li = []
    for j in b:
        if j in a:
            li.append(j)
    for z in a:
        if z in b:
            li.append(z)
    l = sorted(li)
    c = ''   
    for x in l:
        if x not in c:
            c+=x
    print(len(c))

---
+++ common_characters_-II.py
@@ -0,0 +1,28 @@
+m = input()
+m = m.lower()
+n = input()
+n = n.lower()
+
+def two_strings(a):
+    s = ""
+    for i in a:
+        if i !=" ":
+            s+=i
+    return s
+
+a = two_strings(m)
+b = two_strings(n)
+
+li = []
+for j in b:
+    if j in a:
+        li.append(j)
+for z in a:
+    if z in b:
+        li.append(z)
+l = sorted(li)
+c = ''   
+for x in l:
+    if x not in c:
+        c+=x
+print(len(c))
